8dd1eb523d38b9d8c9a6f25a8ee6ad45e4eb5757
Forinarr <shuvalova0723@list.ru>
2026-01-19 17:02:34 +0300

    Demonstrate equals, copyandtoString for Task

--- src/Task.kt
+++ src/Task.kt
@@ -8,3 +8,25 @@ data class Task(
     val priority: Priority,
     val isCompleted: Boolean = false
 )
+fun main() {
+    val task1 = Task("Сделать дз по kotlin", "дедлайн: среда", Priority.LOW)
+    val task2 = Task("сделать дз по C#", "дедлайн: пятница", Priority.MEDIUM)
+    val task3 = Task("Сделать дз по kotlin", "дедлайн: среда", Priority.HIGH)
+
+    println("Tasks:")
+    println(task1)
+    println(task2)
+    println(task3)
+
+    println("Сравнение задач:")
+    println("task1 == task3: ${task1 == task3}")
+    println("task1 == task1.copy(priority = Priority.LOW): ${task1 == task1.copy(priority = Priority.LOW)}")
+
+    val copiedTask = task1.copy(priority = Priority.HIGH)
+    println("Скопированная задача:")
+    println(copiedTask)
+
+    println("HashCode:")
+    println("task1 hashCode: ${task1.hashCode()}")
+    println("copiedTask hashCode: ${copiedTask.hashCode()}")
+}
